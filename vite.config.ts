ab458156302bbe69030842f9f8fe3ff163a054fc
noname-mt <enthusiasticrose@gmail.com>
2025-10-18 13:16:12 +0600

    repository changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/' // for project sites on GitHub Pages; use '/' for user site (username.github.io)
+  base: '/portfolio/' // for project sites on GitHub Pages; use '/' for user site (username.github.io)
 })
